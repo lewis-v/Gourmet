9e438a2afa77e50a09aa48b2e91752816958dde5
lewis-v <605788229@qq.com>
2017-10-23 17:24:30 +0800

    2017_10_22 16:27 首页UI

---
+++ app/src/main/java/com/yw/gourmet/ui/main/FunctionFragment.java
@@ -0,0 +1,26 @@
+package com.yw.gourmet.ui.main;
+
+import com.yw.gourmet.R;
+import com.yw.gourmet.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class FunctionFragment extends BaseFragment{
+    /**
+     * 初始化UI
+     */
+    @Override
+    protected void initView() {
+
+    }
+
+    /**
+     * 设置布局文件
+     */
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_function;
+    }
+}
